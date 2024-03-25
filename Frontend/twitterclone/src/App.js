99b27eed78a0597d45fc5d7fbe4b345bd3938ac7
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-25 17:53:40 +0530

    Created the project boilerplate...

---
+++ Frontend/twitterclone/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App text-xl text-red-200 font-bold">
+      Let's build Twitter Clone...
+    </div>
+  );
+}
+
+export default App;
